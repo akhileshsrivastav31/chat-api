442fbf654536b9bf6f16392604c06467a18e5a96
akhileshsrivastav31 <akhileshsrivastav31@gmail.com>
2024-12-04 21:49:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,36 +6,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://akhileshsiliconstream:<db_password>@cluster0.wgkk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://akhileshsiliconstream:hcl1234@cluster0.wgkk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect("mongodb+srv://akhileshsiliconstream:Hcl12345@cluster0.wgkk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
