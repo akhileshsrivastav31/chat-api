d36ad7686e8ca08e22dce86ce44e7c38576482c3
akhileshsrivastav31 <akhileshsrivastav31@gmail.com>
2024-12-07 10:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
